512b4a7624dadc72ea6690a3bd013155227ae192
Vengatesh <venkuvenky2@gmail.com>
2024-06-08 23:23:25 +0530

    added swagger

--- src/main/java/com/taskx/Spring/Data/MongoDB/SpringDataMongoDbApplication.java
+++ src/main/java/com/taskx/Spring/Data/MongoDB/SpringDataMongoDbApplication.java
@@ -1,9 +1,15 @@
 package com.taskx.Spring.Data.MongoDB;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(title = "Employee Management", version = "v1.1",
+				description = "CRUD Operations of Employee using MongoDB")
+)
 public class SpringDataMongoDbApplication {
 
 	public static void main(String[] args) {
