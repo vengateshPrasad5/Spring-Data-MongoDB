21cc6cf4a1e3f2f603b69151e20c1a61c1efbb00
Vengatesh <venkuvenky2@gmail.com>
2024-06-08 23:12:33 +0530

    initial commit

---
+++ src/main/java/com/taskx/Spring/Data/MongoDB/SpringDataMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.taskx.Spring.Data.MongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataMongoDbApplication.class, args);
+	}
+
+}
