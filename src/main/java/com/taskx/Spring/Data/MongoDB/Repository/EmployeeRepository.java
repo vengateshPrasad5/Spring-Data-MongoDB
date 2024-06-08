557f6beb1925f1c3fd4470f79c04eddddbd66f37
Vengatesh <venkuvenky2@gmail.com>
2024-06-08 23:39:11 +0530

    added query method

--- src/main/java/com/taskx/Spring/Data/MongoDB/Repository/EmployeeRepository.java
+++ src/main/java/com/taskx/Spring/Data/MongoDB/Repository/EmployeeRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepository extends MongoRepository<Employee,String> {
+    Employee findByEmpName(String empName);
 }
