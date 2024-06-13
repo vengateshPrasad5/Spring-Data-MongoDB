7c97d1f582fc953b60cac3bc8b6381d214a920b9
Vengatesh <venkuvenky2@gmail.com>
2024-06-13 16:11:09 +0530

    modified entity

--- src/main/java/com/taskx/Spring/Data/MongoDB/Entity/Employee.java
+++ src/main/java/com/taskx/Spring/Data/MongoDB/Entity/Employee.java
@@ -2,7 +2,6 @@ package com.taskx.Spring.Data.MongoDB.Entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Generated;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -15,6 +14,6 @@ public class Employee {
     @Id
     private String id;
     private String empName;
-    private Long empSalary;
-    private String empDept;
+    private String empEmail;
+    private String empLocation;
 }
